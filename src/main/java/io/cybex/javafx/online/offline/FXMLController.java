a51885d8c9ab814b731e03e3fb9407ceb75cc3e5
cybex237 <cybexsa@gmail.com>
2019-11-22 14:08:45 +0100

    added timeline and connectivity util in controller

--- src/main/java/io/cybex/javafx/online/offline/FXMLController.java
+++ src/main/java/io/cybex/javafx/online/offline/FXMLController.java
@@ -1,25 +1,84 @@
 package io.cybex.javafx.online.offline;
 
+//<editor-fold defaultstate="collapsed" desc="imports">
+import io.cybex.javafx.online.offline.util.ConnectivityUtil;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.concurrent.ScheduledService;
+import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+import javafx.util.Duration;
+//</editor-fold>
 
 public class FXMLController implements Initializable {
-    
+
+//<editor-fold defaultstate="collapsed" desc="private methods">
+    @FXML
+    private Pane offlinePane;
+
     @FXML
     private Label label;
-    
+
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
-    
+    private Pane onlinePane;
+
+    @FXML
+    private Label label1;
+
+    private ConnectivityUtil connectivityUtil = new ConnectivityUtil();
+
+    private Timeline timeline;
+//</editor-fold>
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+        timeline = new Timeline(new KeyFrame(Duration.seconds(5), new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                internetCheck.setPeriod(Duration.seconds(5));
+                internetCheck.restart();
+            }
+        }));
+        timeline.setCycleCount(Timeline.INDEFINITE);
+        timeline.play();
+    }
+
+    //<editor-fold defaultstate="collapsed" desc="internetCheck">
+    ScheduledService<Boolean> internetCheck = new ScheduledService<Boolean>() {
+        @Override
+        protected Task<Boolean> createTask() {
+            Task<Boolean> aliveTask = new Task<Boolean>() {
+                @Override
+                protected Boolean call() throws Exception {
+                    return connectivityUtil.netIsAvailable();
+                }
+
+                @Override
+                protected void succeeded() {
+                    if (getValue()) { 
+
+                        onlinePane.setVisible(true);
+                        offlinePane.setVisible(false);
+                    } else {
+                        
+                        offlinePane.setVisible(true);
+                        onlinePane.setVisible(false);
+                    }
+
+                }
+            };
+            return aliveTask;
+
+        }
+    ;
+};
+//</editor-fold>
 }
